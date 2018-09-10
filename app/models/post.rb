00555e2270b54215b6f813726fee91644e4647ec
Oisin Carroll <carroll.oisin@gmail.com>
2018-09-11 00:12:45 +0100

    Comments on Posts
    Comment Controller
    Comment View
    _view
    Db migrate (Foreign Key)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+
+	has_many :comments, dependent: :destroy
+
+	def create
+		
+	end
+
+	#def destroy
+	#	@post = Post.find(params[:post_id])
+	#end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_212800) do
+ActiveRecord::Schema.define(version: 2018_09_10_221524) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
